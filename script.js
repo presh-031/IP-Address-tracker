31ed8bd341ddffd60be42c2f0ae2dfc373aba1c8
presh-031 <akingbulugbe.dev@gmail.com>
2022-08-30 01:05:01 +0100

    adding fetch-user-ip-details on load functionality

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 "use strict";
+// On initial page load,use should see their own ip address and information
+// use ipify to get user's ip address
+fetch(`https://api.ipify.org/?format=json`)
+  .then((res) => res.json()) //parse response as JSON
+  .then((data) => {
+    console.log(data);
+    const userIp = data.ip;
+    fetchIpDetails(userIp);
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
+
+// User should be able to search for any IP address
 const searchBtn = document.querySelector(".search-btn");
+const searchedIp = document.querySelector("#search-bar").value;
+searchBtn.addEventListener("click", () => {
+  fetchIpDetails(searchedIp);
+});
 
-searchBtn.addEventListener("click", fetchIpDetails);
-
-async function fetchIpDetails() {
-  const userIp = document.querySelector("#search-bar").value;
-  console.log(userIp);
+// fetch ip details
+async function fetchIpDetails(ip) {
   const url = `
-  https://geo.ipify.org/api/v2/country?apiKey=at_bGDoy4LONhpW05ix1qSSs6uK9iZex&ipAddress=${userIp}`;
+  https://geo.ipify.org/api/v2/country?apiKey=at_bGDoy4LONhpW05ix1qSSs6uK9iZex&ipAddress=${ip}`;
 
   const res = await fetch(url);
   const data = await res.json(); //parse response as JSON
-  // console.log(data);
 
   // updating dom with important data
-  document.querySelector(".ip").innerHTML = data.ip;
+  document.querySelector(".ip-address").innerHTML = data.ip;
   document.querySelector(".location").innerHTML = data.location.country + ", " + data.location.region;
   document.querySelector(".timezone").innerHTML = data.location.timezone;
   document.querySelector(".isp").innerHTML = data.isp;

1da0a107b50a16a9b28e0be5514ff8dfd9a3c7b6
presh-031 <akingbulugbe.dev@gmail.com>
2022-08-31 11:49:39 +0100

    map now displays right location on page load and on search

--- README-template.md
+++ README-template.md
@@ -7,9 +7,13 @@ This is a solution to the [IP address tracker challenge on Frontend Mentor](http
 ## Table of contents
 
 - [Overview](#overview)
+  <!-- How to use -->
+  <!-- Features -->
+
   - [The challenge](#the-challenge)
   - [Screenshot](#screenshot)
   - [Links](#links)
+
 - [My process](#my-process)
   - [Built with](#built-with)
   - [What I learned](#what-i-learned)
--- script.js
+++ script.js
@@ -30,20 +30,27 @@ function getUserInput() {
 // fetch ip details
 async function fetchIpDetails(ipAddress) {
   const url = `
-  https://geo.ipify.org/api/v2/country?apiKey=at_bGDoy4LONhpW05ix1qSSs6uK9iZex&ipAddress=${ipAddress}`;
+  https://geo.ipify.org/api/v2/country,city?apiKey=at_bGDoy4LONhpW05ix1qSSs6uK9iZex&ipAddress=${ipAddress}`;
   const res = await fetch(url);
   const data = await res.json(); //parse response as JSON
 
+  const lat = data.location.lat;
+  const lng = data.location.lng;
+
   // updating dom with important data
   document.querySelector(".ip-address").innerHTML = data.ip;
-  document.querySelector(".location").innerHTML = data.location.country + ", " + data.location.region;
+  document.querySelector(".location").innerHTML =
+    data.location.country + ", " + data.location.region + ", " + data.location.city;
   document.querySelector(".timezone").innerHTML = data.location.timezone;
   document.querySelector(".isp").innerHTML = data.isp;
+
+  // calling map function with lat and lng
+  showMap(lat, lng);
 }
 
 // Adding map functionality with leaflet.js
-function showMap() {
-  var map = L.map("map").setView([51.505, -0.09], 13);
+function showMap(lat, lng) {
+  var map = L.map("map").setView([lat, lng], 13);
 
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
@@ -51,4 +58,3 @@ function showMap() {
   }).addTo(map);
 }
 // Get the lat and long of current IP, and call the showMap function with them.
-showMap();
